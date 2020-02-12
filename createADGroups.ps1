c32bd28921fc92f94ac43ed72ad0ad7fdb68d570
ZacBleningerTR <60705726+ZacBleningerTR@users.noreply.github.com>
2020-02-12 13:48:23 -0600

    Create createADGroups.ps1

---
+++ createADGroups.ps1
@@ -0,0 +1,18 @@
+# Set subscription
+
+param(
+[string]$subId,
+[string]$rgName
+)
+
+$ownerADGroupName="MA-$subId-$rgName-Owner"
+$contributorADGroupName="MA-$subId-$rgName-Contributor"
+$readerADGroupName="MA-$subId-$rgName-Reader"
+
+# set Azure subscription
+Set-AzContext -SubscriptionId $subId 
+
+# create AD groups for owner, contributor, and reader
+New-AzADGroup -DisplayName $ownerADGroupName -MailNickname "$ownerADGroupName-mail"
+New-AzADGroup -DisplayName $contributorADGroupName -MailNickname "$contributorADGroupName-mail"
+New-AzADGroup -DisplayName $readerADGroupName -MailNickname "$readerADGroupName-mail"
