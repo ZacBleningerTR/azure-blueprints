18b1efe93fcc0e5a2e9952565c4dea6b4e0b7604
unknown <zac.bleninger@thomsonreuters.com>
2020-02-18 12:05:27 -0600

    Update mail nickname

--- resource-group-inflation/createADGroups.ps1
+++ resource-group-inflation/createADGroups.ps1
@@ -13,8 +13,8 @@ $readerADGroupName="MA-$subId-$rgName-Reader"
 Set-AzContext -SubscriptionId $subId 
 
 # create AD groups for owner, contributor, and reader
-New-AzADGroup -DisplayName $ownerADGroupName -MailNickname "$ownerADGroupName-mail"
-New-AzADGroup -DisplayName $contributorADGroupName -MailNickname "$contributorADGroupName-mail"
-New-AzADGroup -DisplayName $readerADGroupName -MailNickname "$readerADGroupName-mail"
+New-AzADGroup -DisplayName $ownerADGroupName -MailNickname "$ownerADGroupNametest1"
+New-AzADGroup -DisplayName $contributorADGroupName -MailNickname "$contributorADGroupNametest1"
+New-AzADGroup -DisplayName $readerADGroupName -MailNickname "$readerADGroupNametest1"
 
 
