2caf93cf00a9e7a28b4fa99f01ed7bd8b8b12cd9
Zac Bleninger <zac.bleninger@thomsonreuters.com>
2020-02-24 21:35:07 -0600

    Update createADGroups.sh to remove previous commit

--- resource-group-inflation/createADGroups.sh
+++ resource-group-inflation/createADGroups.sh
@@ -20,15 +20,3 @@ az account set -s $subscription_id
 az ad group create --display-name $owner_ad_group --mail-nickname $owner_ad_group --force false
 az ad group create --display-name $contributor_ad_group --mail-nickname $contributor_ad_group --force false
 az ad group create --display-name $reader_ad_group --mail-nickname $reader_ad_group --force false
-
-# retrieve custom contributor and owner role templates
-curl https://raw.githubusercontent.com/ZacBleningerTR/azure-blueprints/master/resource-group-inflation/Custom-App-Contributor.json > Custom-App-Contributor.json
-curl https://raw.githubusercontent.com/ZacBleningerTR/azure-blueprints/master/resource-group-inflation/Custom-App-Owner.json > Custom-App-Owner.json
-
-# replace parameter with subscription id
-sed -e 's/%sub%/$subscription_id/g' Custom-App-Contributor.json
-sed -e 's/%sub%/$subscription_id/g' Custom-App-Owner.json
-
-# create role definitions
-az role definition create --role-definition @Custom-App-Contributor.json
-az role definition create --role-definition @Custom-App-Owner.json
