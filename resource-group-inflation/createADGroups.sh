a65f31544f8f3356d52179295d6f60aaa8e1f4a6
Zac Bleninger <zac.bleninger@thomsonreuters.com>
2020-02-24 21:47:48 -0600

    Add role assignemnts to the createADGroups.sh script

--- resource-group-inflation/createADGroups.sh
+++ resource-group-inflation/createADGroups.sh
@@ -20,3 +20,13 @@ az account set -s $subscription_id
 az ad group create --display-name $owner_ad_group --mail-nickname $owner_ad_group --force false
 az ad group create --display-name $contributor_ad_group --mail-nickname $contributor_ad_group --force false
 az ad group create --display-name $reader_ad_group --mail-nickname $reader_ad_group --force false
+
+# retrieve AD group object IDs
+owner_object_id=$(az ad group show --group $owner_ad_group)
+contributor_object_id=$(az ad group show --group $contributor_ad_group)
+reader_object_id=$(az ad group show --group $reader_ad_group)
+
+# assign custom roles to AD groups
+az role assignment create --role "Custom-App-Owner-$subscription_id" --resource-group $resource_group --assignee-object-id $owner_object_id
+az role assignment create --role "Custom-App-Contributor-$subscription_id" --resource-group $resource_group --assignee-object-id $contributor_object_id
+az role assignment create --role "Reader" --resource-group $resource_group --assignee-object-id $reader_object_id
