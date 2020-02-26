32344e1be98a2cb2caa146d345376ecae0945b05
Zac Bleninger <zac.bleninger@thomsonreuters.com>
2020-02-26 11:11:31 -0600

    Add temp fix to mail-nickname with hardcoded cut length

--- resource-group-inflation/createADGroups.sh
+++ resource-group-inflation/createADGroups.sh
@@ -16,10 +16,16 @@ reader_ad_group="MA-$subscription_id-$resource_group-Owner"
 # set Azure subscription
 az account set -s $subscription_id
 
+# ${#myvar} use this to get string length to get exact end of cut length 
+
+owner_ad_group_mail="$(echo $owner_ad_group | cut -c41-100)-mail"
+contributor_ad_group_mail="$(echo $owner_ad_group | cut -c41-100)-mail"
+reader_ad_group_mail="$(echo $owner_ad_group | cut -c41-100)-mail"
+
 # create AD groups for owner, contributor, and reader
-az ad group create --display-name "$owner_ad_group" --mail-nickname "$owner_ad_group" --force false
-az ad group create --display-name "$contributor_ad_group" --mail-nickname "$contributor_ad_group" --force false
-az ad group create --display-name "$reader_ad_group" --mail-nickname "$reader_ad_group" --force false
+az ad group create --display-name "$owner_ad_group" --mail-nickname "$owner_ad_group_mail" --force false
+az ad group create --display-name "$contributor_ad_group" --mail-nickname "$contributor_ad_group_mail" --force false
+az ad group create --display-name "$reader_ad_group" --mail-nickname "$reader_ad_group_mail" --force false
 
 ## can we out put principal IDs of above to allow us to pass to role assignment ARM template?
 
